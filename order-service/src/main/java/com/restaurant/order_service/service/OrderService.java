cb7a7b7d2e495d4736324875bfe99c6c877bd287
Rafael Gonçalves <spdgrafael@gmail.com>
2025-02-25 19:47:32 +0000

    Method Implementation for ready orders

--- order-service/src/main/java/com/restaurant/order_service/service/OrderService.java
+++ order-service/src/main/java/com/restaurant/order_service/service/OrderService.java
@@ -4,12 +4,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.restaurant.order_service.dto.ClientResponse;
 import com.restaurant.order_service.feignClient.ClientServiceFeignClient;
 import com.restaurant.order_service.model.Order;
 import com.restaurant.order_service.model.OrderStatus;
+import com.restaurant.order_service.model.OrderUpdate;
 import com.restaurant.order_service.repository.OrderRepository;
 
 @Service
@@ -17,11 +21,13 @@ public class OrderService {
     
     private final OrderRepository orderRepository;
     private final ClientServiceFeignClient clientServiceFeignClient;
+    private final RabbitTemplate rabbitTemplate;
 
     
-    public OrderService(OrderRepository orderRepository, ClientServiceFeignClient clientServiceFeignClient) {
+    public OrderService(OrderRepository orderRepository, ClientServiceFeignClient clientServiceFeignClient, RabbitTemplate rabbitTemplate) {
         this.orderRepository = orderRepository;
         this.clientServiceFeignClient = clientServiceFeignClient;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     public List<Order> getAllOrders() {
@@ -54,5 +60,20 @@ public class OrderService {
         order.setStatus(status);
 
         return orderRepository.save(order);
+    }
+
+    public void notifyOrderReady(Long orderId) {
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String message = objectMapper.writeValueAsString(new OrderUpdate(orderId, OrderStatus.READY));
+
+            rabbitTemplate.convertAndSend("orderExchange", "order.ready", message);
+            System.out.println("Message send to RabbitMQ: Pedido " +  orderId + " está pronto.");
+        } catch (JsonProcessingException e) {
+            System.err.println("Error converting message to JSON: " + e.getMessage());
+        }
+
+        
     }
 }
